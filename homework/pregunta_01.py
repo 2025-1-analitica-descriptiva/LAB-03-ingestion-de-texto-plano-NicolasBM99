ff51ad66cbda55928922750c87a834a42f912be7
NicolasBM99 <nbenjumea@unal.edu.co>
2025-06-22 19:52:31 -0500

    Intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -4,6 +4,8 @@ Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 
 # pylint: disable=import-outside-toplevel
 
+import pandas as pd
+import re
 
 def pregunta_01():
     """
@@ -18,3 +20,40 @@ def pregunta_01():
 
 
     """
+    with open("files/input/clusters_report.txt", encoding="utf-8") as file:
+        lines = file.readlines()[4:] 
+
+    bloques = []
+    bloque = ""
+    for line in lines:
+        if re.match(r"\s+\d+\s+", line): 
+            if bloque:
+                bloques.append(bloque)
+            bloque = line
+        else:
+            bloque += line
+    bloques.append(bloque)
+
+    data = []
+    for bloque in bloques:
+        partes = re.split(r"\s{2,}", bloque.strip())
+        cluster = int(partes[0])
+        cantidad = int(partes[1])
+        porcentaje = float(partes[2].replace(",", ".").replace(" %", ""))
+        palabras = " ".join(partes[3:]) 
+        palabras = re.sub(r"\s+", " ", palabras).strip().replace(".,", ".").replace(" .", ".")
+        palabras = palabras.replace(".","")
+        data.append([cluster, cantidad, porcentaje, palabras])
+
+    df = pd.DataFrame(data, columns=[
+        "cluster",
+        "cantidad_de_palabras_clave",
+        "porcentaje_de_palabras_clave",
+        "principales_palabras_clave"
+    ])
+
+    return df
+
+if __name__ == "__main__":
+    df = pregunta_01()
+    print(df.head())
